0f2c84b173c153e8ffa8f9f13253f634e1e09f4a
Neroshan Sivapalasundram <neroshan12@gmail.com>
2017-08-16 16:28:39 +0100

    Add corrected version of typos file (8.11)

---
+++ typos.rb
@@ -0,0 +1,35 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []                                    # create an empty array
+  name = gets.chomp                                # get the first name
+
+  while !name.empty? do                            # while the name is not empty, repeat this code
+    students << {name: name, cohort: :november}    # add the student hash to the array
+    puts "Now we have #{students.count} students"
+    name = gets.chomp                              # get another name from the user
+  end
+  students                                         # return the array of students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts ("#{student[:name]}, #{student[:cohort]} cohort")
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
