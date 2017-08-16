b3ad0a84639461d9edf4d49d14d56e7b3e4a50c9
Neroshan Sivapalasundram <neroshan12@gmail.com>
2017-08-16 11:53:18 +0100

    Add each_with_index method (8.1)

--- directory.rb
+++ directory.rb
@@ -3,16 +3,16 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []  # create an empty array
+  students = []                                   # create an empty array
 
-  name = gets.chomp   #get the first name
+  name = gets.chomp                               #get the first name
 
-  while !name.empty? do       # if name is not empty then execute the do block, if not jump to line 17
-    students << {name: name, cohort: :november}  # add the student hash to the array
+  while !name.empty? do                              # if name is not empty then execute the do block, if not jump to line 17
+    students << {name: name, cohort: :november}      # add the student hash to the array
     puts "Now we have #{students.count} students"
-    name = gets.chomp   # get another name from the user
+    name = gets.chomp                                 # get another name from the user
   end
-  students    # return the array of students
+  students                                            # return the array of students
 end
 
 def print_header
@@ -21,16 +21,17 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-# nothing happens until we call the methods
+
 students = input_students
+# nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
