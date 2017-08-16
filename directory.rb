bf782cbbebfc05da5d9af9732114e372bac87932
Neroshan Sivapalasundram <neroshan12@gmail.com>
2017-08-16 12:08:33 +0100

    Add modification for printing names shorter than 12 characters (8.3)

--- directory.rb
+++ directory.rb
@@ -22,7 +22,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].capitalize == "N"
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].capitalize == "N" && student[:name].length < 12
   end
 end
 
