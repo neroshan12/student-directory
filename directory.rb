a9b43fa17581c3a98639a16d46e4b8753c01f40d
Neroshan Sivapalasundram <neroshan12@gmail.com>
2017-08-16 10:53:52 +0100

    Add user interaction

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
 # let's puts all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []  # create an empty array
+
+  name = gets.chomp   #get the first name
+
+  while !name.empty? do       # if name is not empty then execute the do block, if not jump to line 17
+    students << {name: name, cohort: :november}  # add the student hash to the array
+    puts "Now we have #{students.count} students"
+    name = gets.chomp   # get another name from the user
+  end
+  students    # return the array of students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +30,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
