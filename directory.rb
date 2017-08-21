26a9698350b1d3a34dfa15f4b9a37932859e6ec5
Neroshan Sivapalasundram <neroshan12@gmail.com>
2017-08-21 10:51:36 +0100

    Refactor code (14.3)

--- directory.rb
+++ directory.rb
@@ -1,6 +1,7 @@
 @students = [] # an empty array accessible to all methods
 
 def print_menu
+  puts "Please choose from the following menu: "
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
@@ -17,16 +18,11 @@ end
 
 def process(selection)
   case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit # this will cause the program to terminate
+    when "1" then input_students
+    when "2" then show_students
+    when "3" then save_students
+    when "4" then load_students
+    when "9" then exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
   end
